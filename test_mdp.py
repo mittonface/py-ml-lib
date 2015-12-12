dfd874fd97babb40d3510e744fcc0580b15b8111
Brent Mitton <bmitton@gatech.edu>
2015-12-12 16:59:17 -0400

    more basic mdp tests

--- test_mdp.py
+++ test_mdp.py
@@ -15,6 +15,8 @@ class TestMDP(TestCase):
         mdp = MDP()
         self.assertEqual(mdp.size(), 0)
 
+        # You should be able to init an MDP with the number of states that you want
+        # it to have, I guess.
         mdp = MDP(5)
         self.assertEqual(mdp.size(), 5)
 
@@ -29,6 +31,8 @@ class TestMDP(TestCase):
         connecting them afterwards by specifying the actions.
         """
 
+        # States should be abled to be identified by numbers or by strings I suppose.
+        # I don't imagine that strings will ever be used.
         mdp = MDP()
         mdp.add_state(0)
         mdp.add_state(1)
@@ -38,6 +42,7 @@ class TestMDP(TestCase):
         mdp.add_state(5, terminal=True)
         self.assertEqual(mdp.size(), 5)
 
+        # Test that it works for strings anyway? Named States?
         mdp = MDP()
         mdp.add_state("StateOne")
         mdp.add_state("StateTwo")
@@ -49,10 +54,13 @@ class TestMDP(TestCase):
         """
         Should probably be some nice way to get states by a state id or state name
         """
+
+        # test that you can get a state by numerical id
         mdp = MDP()
         mdp.add_state(0)
         self.assertEquals(type(mdp.get_state(1)), State)
 
+        # test that you can get a state by string id
         mdp = MDP()
         mdp.add_state("Test")
         self.assertEquals(type(mdp.get_state("Test"), State))
@@ -65,10 +73,31 @@ class TestMDP(TestCase):
         """
         Probably nice to be able to tell the size of the MDP. If not only for tests
         """
+        mdp = MDP()
+        self.assertEqual(mdp.size(), 0)
+
+        mdp = MDP(5)
+        self.assertEqual(mdp.size(), 5)
+
+        mdp = MDP()
+        mdp.add_state(0)
+        mdp.add_state(1)
+        self.assertEqual(mdp.size(), 2)
+        pass
 
     def test_add_action(self):
         """
-        Every state should share a set of actions
+
+        """
+        pass
+
+    def test_get_action(self):
+        pass
+
+    def test_get_num_actions(self):
+        """
+        It will be helpful to be able to return the number of distinct actions that an MDP
+        has.
         """
         pass
 
